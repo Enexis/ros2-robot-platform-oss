53f87db9983a9dcdbb0bb136ec2781725e246406
Berend Pijnenburg <berend.pijnenburg@student.fontys.nl>
2026-05-20 10:54:16 +0200

    fix: go2 bringup removed controller spawns when using physical hardware. Physical hardware makes use of another driver so these will not be used. Also formatted and added some comments for clarity.

--- robotics_modules/mobile_bases/unitree_go2/go2_bringup/launch/go2.launch.py
+++ robotics_modules/mobile_bases/unitree_go2/go2_bringup/launch/go2.launch.py
@@ -24,81 +24,95 @@ from launch.substitutions import LaunchConfiguration, PythonExpression
 
 
 def generate_launch_description():
-    rviz = LaunchConfiguration('rviz')
+    rviz = LaunchConfiguration("rviz")
     use_sim_time = LaunchConfiguration("use_sim_time")
 
     declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description='Namespace for the robot'
+        "namespace", default_value="", description="Namespace for the robot"
     )
 
     declare_rviz_cmd = DeclareLaunchArgument(
-        'rviz',
-        default_value='False',
-        description='Launch rviz'
+        "rviz", default_value="False", description="Launch rviz"
     )
 
     robot_description_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('go2_description'),
-            'launch/'), 'robot.launch.py']),
-            launch_arguments={'namespace': LaunchConfiguration('namespace')}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("go2_description"), "launch/"),
+                "robot.launch.py",
+            ]
+        ),
+        launch_arguments={"namespace": LaunchConfiguration("namespace")}.items(),
     )
 
-    driver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('go2_driver'),
-            'launch/'), 'go2_driver.launch.py']),
-            launch_arguments={'namespace': LaunchConfiguration('namespace')}.items(),
-            condition=IfCondition(PythonExpression(["not ", use_sim_time]))
-        )
-    
-
-    controller_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('unitree_go2_controller'),
-            'launch/'), 'controller.launch.py']),
-            launch_arguments={'namespace': LaunchConfiguration('namespace')}.items(),
-            condition=IfCondition(PythonExpression([use_sim_time]))
+    # When not simulating use the driver that interacts with the unitree go2 hardware
+    phycical_driver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("go2_driver"), "launch/"),
+                "go2_driver.launch.py",
+            ]
+        ),
+        launch_arguments={"namespace": LaunchConfiguration("namespace")}.items(),
+        condition=IfCondition(PythonExpression(["not ", use_sim_time])),
     )
 
-    rviz_cmd = Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=['-d',
-                       os.path.join(get_package_share_directory('go2_description'),
-                                    'config', 'go2_rviz.rviz')],
-            condition=IfCondition(PythonExpression([rviz]))
+    # When simulating use a CHAMP unitree go2 controller with ROS2 controllers
+    sim_controller_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("unitree_go2_controller"), "launch/"
+                ),
+                "controller.launch.py",
+            ]
+        ),
+        launch_arguments={"namespace": LaunchConfiguration("namespace")}.items(),
+        condition=IfCondition(PythonExpression([use_sim_time])),
     )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        namespace=LaunchConfiguration('namespace'),
-        arguments=['joint_state_broadcaster'],
+        namespace=LaunchConfiguration("namespace"),
+        arguments=["joint_state_broadcaster"],
+        condition=IfCondition(PythonExpression([use_sim_time])),
     )
 
     joint_trajectory_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        namespace=LaunchConfiguration('namespace'),
+        namespace=LaunchConfiguration("namespace"),
         arguments=["joint_group_effort_controller"],
+        condition=IfCondition(PythonExpression([use_sim_time])),
+    )
+    ############################################################################
+
+    rviz_cmd = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}],
+        arguments=[
+            "-d",
+            os.path.join(
+                get_package_share_directory("go2_description"),
+                "config",
+                "go2_rviz.rviz",
+            ),
+        ],
+        condition=IfCondition(PythonExpression([rviz])),
     )
 
     ld = LaunchDescription()
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_rviz_cmd)
     ld.add_action(robot_description_cmd)
-    ld.add_action(driver_cmd)
-    ld.add_action(rviz_cmd)
-    ld.add_action(controller_cmd)
-    
+    ld.add_action(phycical_driver_cmd)
+    ld.add_action(sim_controller_cmd)
     ld.add_action(joint_state_broadcaster_spawner)
     ld.add_action(joint_trajectory_controller_spawner)
-
+    ld.add_action(rviz_cmd)
 
     return ld
